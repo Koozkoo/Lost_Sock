e6361df167e8c4c573b250a507df6c8ee27d6428
1Murphy1 <115133958+1Murphy1@users.noreply.github.com>
2023-04-11 12:39:30 +0700

    cat and sock

---
+++ game/cod/pers/Sock.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+const SPEED =100
+const FLOOR= Vector2(0,-1)
+const GRAVITY=970
+const JUMP_POWER=300
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	if position.y>800:
+		position.y=0
+	if Input.is_action_pressed("ui_right"):
+		velocity.x=SPEED
+		$AnimatedSprite.flip_h= false
+		if is_on_floor():
+			$AnimatedSprite.play("run")
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x=-SPEED
+		$AnimatedSprite.flip_h= true
+		if is_on_floor():
+			$AnimatedSprite.play("run")
+	else:
+		velocity.x=0
+		if is_on_floor():
+			$AnimatedSprite.play("idle")
+		
+	if Input.is_action_pressed("ui_up") && is_on_floor():
+		velocity.y= -JUMP_POWER
+		$AnimatedSprite.play("jump ")
+	velocity.y+=(GRAVITY * delta)
+	velocity= move_and_slide(velocity, FLOOR)
